160f988b56a8dc4bf68e97a81c6311f23010e8de
Eric Perez Vera <ericperezvera02@gmail.com>
2025-07-30 12:24:48 +0200

    implementamos dialogo en riddle room con i18n sin api

--- src/pages/api/ping.ts
+++
@@ -1,7 +0,0 @@
-import type { APIRoute } from 'astro';
-
-export const get: APIRoute = () => {
-  return new Response(JSON.stringify({ pong: true }), {
-    headers: { 'Content-Type': 'application/json' },
-  });
-};
